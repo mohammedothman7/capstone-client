c980cf3064b8b178f0f082bb18279888b846d12a
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-16 15:36:15 -0400

    Added Switch and Route to RoutesView to call AllGamesContainer when going to path '/'

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { AllGamesContainer } from '../containers';
 
-const RoutesView = () => {};
+const RoutesView = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={AllGamesContainer}></Route>
+    </Switch>
+  );
+};
 
 export default RoutesView;
