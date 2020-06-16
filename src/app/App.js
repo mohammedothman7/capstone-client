0d25029127e7f943d83ccd400d20ca9e816f4cff
Alan Eappen <eappenalan123@gmail.com>
2020-06-16 14:43:09 -0400

    Added Navbar

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import { NavBarContainer } from "../components/containers";
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return(
+      <div>
+        <NavBarContainer />
+      </div>
+    );
   }
 }
 
