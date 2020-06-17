f6fb2bd408e82fa04f301707db72254812e892fe
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-17 12:04:07 -0400

    Fixed merge conflict

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -1,22 +1,19 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import {
   LoginFormContainer,
   RegisterFormContainer,
-<<<<<<< HEAD
   AboutContainer,
-=======
   AllGamesContainer,
->>>>>>> e87118538da2d0bc900b4298eaaefa66cec62c2f
-} from "../containers";
+} from '../containers';
 
 const RoutesView = () => {
   return (
     <Switch>
-      <Route exact path="/" component={AllGamesContainer}></Route>
-      <Route exact path="/register" component={RegisterFormContainer} />
-      <Route exact path="/login" component={LoginFormContainer} />
-      <Route exact path="/about" component={AboutContainer} />
+      <Route exact path='/' component={AllGamesContainer}></Route>
+      <Route exact path='/register' component={RegisterFormContainer} />
+      <Route exact path='/login' component={LoginFormContainer} />
+      <Route exact path='/about' component={AboutContainer} />
     </Switch>
   );
 };
