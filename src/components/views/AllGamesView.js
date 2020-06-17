329c779d5853ccdb41451a3b570b7631e3f54ab6
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-17 15:22:09 -0400

    Only the games from the API response is stored in games array, instead of the whole response from API, so took out redundant 'data' attribute for mapping over games array

--- src/components/views/AllGamesView.js
+++ src/components/views/AllGamesView.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
 function AllGamesView(props) {
   return (
     <div>
       <button onClick={props.handleFilter({ page: -2 })}>Previous Page</button>
       <button onClick={props.handleFilter({ page: -1 })}>Next Page</button>
-      {props.games.data ? (
-        props.games.data.map((game) => {
+      {props.games ? (
+        props.games.map((game) => {
           return (
             <div key={game.id}>
               <h1>{game.name}</h1>
               <img
                 alt={game.name}
                 src={game.background_image}
-                style={{ maxWidth: "200px", maxHeight: "200px" }}
+                style={{ maxWidth: '200px', maxHeight: '200px' }}
               />
             </div>
           );
