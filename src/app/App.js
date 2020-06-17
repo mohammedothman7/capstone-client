a31ec6d3e8832783fc55e975c248c73320edf708
Alan Eappen <eappenalan123@gmail.com>
2020-06-17 11:35:50 -0400

    changes a few codes

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import RoutesContainer from "../components/routes/RoutesContainer";
-import "./App.css";
 import { NavBarContainer } from "../components/containers";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <NavBarContainer />
-      <header>
-        <RoutesContainer />
-      </header>
+      <div className="app">
+        <NavBarContainer />
+        <header>
+          <RoutesContainer />
+        </header>
       </div>
     );
   }
