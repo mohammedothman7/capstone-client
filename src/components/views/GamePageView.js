d5145c77542189a6e8062471cd754691c11f31ab
Jan Markus Milan <jmarkusm@me.com>
2020-06-19 02:52:19 -0400

    Added some minor styling to GamePage

--- src/components/views/GamePageView.js
+++ src/components/views/GamePageView.js
@@ -1,21 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./styles/GamePageView.css";
 
 const GamePageView = (props) => {
   return (
     <div className="game-info">
       {props.game.name ? (
         <div>
-          <Link to="/">
-            <button>Home page</button>
-          </Link>
-          <img src={props.game.background_image} alt={props.game.name}></img>
-          <h1>game: {props.game.name}</h1>
-          <h5>released date: {props.game.released}</h5>
-          <h5>
-            website: <a href={props.game.website}>{props.game.website}</a>
-          </h5>
-          <p>{props.game.description_raw}</p>
+          <div className="container">
+            <div className="row mt-5">
+              <div className="col-12">
+                <img
+                  src={props.game.background_image}
+                  className="d-block w-100"
+                  alt="Game Image"
+                />
+              </div>
+            </div>
+            <div className="row mt-5">
+              <div className="col-4">
+                <div>
+                  <h3 className="text-danger">{props.game.name}</h3>
+                  <h5>Released: {props.game.released}</h5>
+                  <h5>
+                    <a href={props.game.website}>{props.game.website}</a>
+                  </h5>
+                  <h6 className="mt-5">Rating: {props.game.rating} / 3</h6>
+                  <div className="progress">
+                    <div
+                      className="progress-bar bg-danger bar-93"
+                      role="progressbar"
+                      aria-valuenow="75"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    >
+                      93 / 100
+                    </div>
+                  </div>
+                  <h6 className="mt-2">
+                    Metacritic: {props.game.metacritic} / 100
+                  </h6>
+                  <div className="progress">
+                    <div
+                      className="progress-bar bg-danger bar-75"
+                      role="progressbar"
+                      aria-valuenow="100"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    >
+                      75 / 100
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-8">
+                <p>{props.game.description_raw}</p>
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="loader"></div>
