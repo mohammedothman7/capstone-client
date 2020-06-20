d837f616d7d2b8e6ea46ef399ec512c33b691003
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-20 16:55:43 -0400

    Added functionality to properly show loading screen when waiting for API to respond and cleaned up code

--- src/components/containers/AllGamesContainer.js
+++ src/components/containers/AllGamesContainer.js
@@ -7,6 +7,7 @@ export class AllGamesContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: false,
       games: [],
       filter: {
         page: 1,
@@ -17,42 +18,63 @@ export class AllGamesContainer extends Component {
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    const { page, page_size } = this.state.filter;
+
     // Call thunk to fetch games from API
+    await this.props.fetchAllGames({ page, page_size });
 
-    this.props.fetchAllGames(this.state.filter);
+    this.setState({ games: this.props.allGames });
+  }
 
-    // Store all games from redux store in games variable
-    const games = this.props.allGames;
-    // Add the games we got from the store to state, so it can be rendered.
-    this.setState({ games });
+  componentDidUpdate(prevProps) {
+    if (prevProps.allGames !== this.props.allGames) {
+      this.setState({ games: this.props.allGames });
+    }
   }
 
   // Sends the params to state, so it can be sent to backend for API call
-  handleFilter = (filter) => (event) => {
+  handleFilter = (filter) => async (event) => {
     // Prevent the page from reloading when called
     event.preventDefault();
 
     /*Create a copy of filter in state, so we can 
      modify if need be and send to fetchAllGamesThunk 
     so we can use in API call */
+    let param = this.state.filter;
 
-    const param = this.state.filter;
+    if (filter.ordering) {
+      param.ordering = filter.ordering;
+    } else {
+      param.ordering = "";
+    }
+    if (filter.dates) {
+      param.dates = filter.dates;
+    } else {
+      param.dates = "";
+    }
 
     /* If next button is clicked then filter.page is set to -1, 
     so we know to increment the page counter by 1 */
-
     if (filter.page === -1) {
       // Modify page in the copy of the state
-      param["page"] = param.page + 1;
+
+      param.page = param.page + 1;
     } else if (filter.page === -2) {
       if (param.page <= 1) return;
       // Decrement page if equal to -2
-      param["page"] = param.page - 1;
+      param.page = param.page - 1;
     }
 
+    this.setState({ isLoading: true });
     // Call Thunk to fetch games from API
-    this.props.fetchAllGames(param);
+    await this.props.fetchAllGames(param);
+    // Set the games array to allGames from redux store
+    this.setState({
+      games: this.props.allGames,
+      isLoading: false,
+      filter: param,
+    });
   };
 
   handleGenre = (event) => {
@@ -63,11 +85,12 @@ export class AllGamesContainer extends Component {
     return (
       <div>
         <AllGamesView
-          games={this.props.allGames}
+          games={this.state.games}
           filter={this.state.filter}
           fetchAllGames={this.props.fetchAllGames}
           handleFilter={this.handleFilter}
           handleGenre={this.handleGenre}
+          isLoading={this.state.isLoading}
         ></AllGamesView>
       </div>
     );
